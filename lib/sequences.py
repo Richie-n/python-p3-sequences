069c98119e1c5c57749fb3d2cdf5ce7799525f41
Richard <ngangarichard335@gmail.com>
2024-05-26 21:33:59 +0300

    done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_seq = []
+    if length > 0:
+        fibonacci_seq.append(0)
+    if length >= 2:
+        fibonacci_seq.append(1)
+        for i in range(2, length):
+            fibonacci_seq.append(fibonacci_seq[-1] + fibonacci_seq[-2])
+
+    print(fibonacci_seq)
+    
+print_fibonacci(10)  
